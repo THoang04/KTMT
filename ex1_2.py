a0291d54c6380094a2b72c46f9af26ae36962912
THoang04 <115413221+THoang04@users.noreply.github.com>
2023-05-28 21:05:48 +0700

    Add files via upload

---
+++ ex1_2.py
@@ -0,0 +1,27 @@
+import RPi.GPIO as GPIO
+import time
+import os
+def main():
+    LED = 13
+    LCD_RS = 23
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(LED, GPIO.OUT)
+    GPIO.setup(LCD_RS, GPIO.OUT)
+    while True:
+        if GPIO.input(LED) == GPIO.LOW:
+            GPIO.output(LED, GPIO.HIGH)
+            time.sleep(1)
+        if GPIO.input(LED) == GPIO.HIGH:
+            GPIO.output(LED, GPIO.LOW)
+            time.sleep(1)
+        if GPIO.input(LCD_RS) == GPIO.LOW:
+            GPIO.output(LCD_RS, GPIO.HIGH)
+            time.sleep(5)
+        if GPIO.input(LCD_RS) == GPIO.HIGH:
+            GPIO.output(LCD_RS, GPIO.LOW)
+            time.sleep(5)
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        GPIO.cleanup()
